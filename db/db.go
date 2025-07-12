996eee3bf12f87470f53abfacebcb2ffdf2a447b
Bishal Shrestha <bishal.shrestha1@infodev.com.np>
2025-07-12 18:03:26 +0545

    retry db connection

--- db/db.go
+++ db/db.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -30,15 +31,18 @@ func Connect() *DB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
 		host, user, password, dbname, port)
 
-	gormDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+	var gormDB *gorm.DB
+	for i := 0; i < 5; i++ {
+		gormDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			return &DB{Gorm: gormDB}
+		}
+		log.Printf("DB connection failed. Retrying in 5s (%d/5)...", i+1)
+		time.Sleep(5 * time.Second)
 	}
 
-	// uncomment this to see the full gorm log, only use for debugging
-	// gormDB = gormDB.Debug()
-
-	return &DB{Gorm: gormDB}
+	log.Fatal("Failed to connect to database after 5 attempts:", err)
+	return nil
 }
 
 func (db *DB) AutoMigrate() {
